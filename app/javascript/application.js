6f1962e7e068304e3fefa2ecdff7bf5eef0903c1
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-03-22 18:08:54 -0300

    automatted commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -13,16 +13,17 @@ require("@rails/ujs").start()
 import './channels/**/*_channel.js'
 import "./controllers"
 
-import * as bootstrap from "bootstrap"
+import * as tailwind from "tailwindcss"
+import 'preline'
 
 document.addEventListener("turbo:load", () => {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
+  // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  //   return new bootstrap.Tooltip(tooltipTriggerEl)
+  // })
 
-  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-  })
+  // var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  // var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  //   return new bootstrap.Popover(popoverTriggerEl)
+  // })
 })
